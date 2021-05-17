165236b2685b09a349987322efca43c7f454998e
YS-McGee <metalonly40@gmail.com>
2021-05-17 22:29:31 +0800

    mod

--- HW3/server_threads.c
+++ HW3/server_threads.c
@@ -167,17 +167,17 @@ int main(int argc, char **argv){
 	int port = atoi(argv[1]);
 	int option = 1;
 	int listenfd = 0, connfd = 0;
-  struct sockaddr_in serv_addr;
-  struct sockaddr_in cli_addr;
-  pthread_t tid;
+  	struct sockaddr_in serv_addr;
+  	struct sockaddr_in cli_addr;
+  	pthread_t tid;
 
-  /* Socket settings */
-  listenfd = socket(AF_INET, SOCK_STREAM, 0);
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = inet_addr(ip);
-  serv_addr.sin_port = htons(port);
+  	/* Socket settings */
+  	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  	serv_addr.sin_family = AF_INET;
+  	serv_addr.sin_addr.s_addr = inet_addr(ip);
+  	serv_addr.sin_port = htons(port);
 
-  /* Ignore pipe signals */
+  	/* Ignore pipe signals */
 	signal(SIGPIPE, SIG_IGN);
 
 	if(setsockopt(listenfd, SOL_SOCKET,(SO_REUSEPORT | SO_REUSEADDR),(char*)&option,sizeof(option)) < 0){
@@ -186,15 +186,15 @@ int main(int argc, char **argv){
 	}
 
 	/* Bind */
-  if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-    perror("ERROR: Socket binding failed");
-    return EXIT_FAILURE;
-  }
+  	if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+    	perror("ERROR: Socket binding failed");
+    	return EXIT_FAILURE;
+  	}
 
-  /* Listen */
-  if (listen(listenfd, 10) < 0) {
-    perror("ERROR: Socket listening failed");
-    return EXIT_FAILURE;
+  	/* Listen */
+  	if (listen(listenfd, 10) < 0) {
+    	perror("ERROR: Socket listening failed");
+    	return EXIT_FAILURE;
 	}
 
 	printf("=== WELCOME TO THE CHATROOM ===\n");
